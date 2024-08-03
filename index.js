face8e7746296d431c5b777a5ac244891bd35baa
gurmeet1011 <gurmeetrathorjnv1012@gmail.com>
2024-08-03 15:44:20 +0530

    #ese hi

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
-const express = require('express')//import
+const express = require('express')
+//import express from "express"
 const app = express()
 const port = 4000 
 
@@ -12,7 +13,6 @@ app.get('/twitter',(req,res)=>{
 app.get('/login',(req,res)=>{
     res.send('<h1>No login Required</h1>');
 })
-app.get('/setup')
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
